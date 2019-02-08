5fbe5d736d36a21f19210301d070589479790358
Victor Machado <joelvictor2012@gmail.com>
2019-02-08 01:50:25 -0300

    Corrections

--- server/routes/instituicao.js
+++ server/routes/instituicao.js
@@ -4,4 +4,5 @@ module.exports = (route) => {
     route.get('/instituicao', route.isAuthenticated, instituicoes.list);
     route.post('/instituicao', route.isAuthenticated, instituicoes.create);
     route.put('/instituicao/:id', route.isAuthenticated, instituicoes.update);
+    route.delete('/instituicao/:id', route.isAuthenticated, instituicoes.remove);
 }
